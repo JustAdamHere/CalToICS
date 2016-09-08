c2eae4c2094a6518fabdc30e36884665a67724b5
JustAdamHere <adam@mrmindstorm.co.uk>
2016-09-08 22:38:18 +0100

    0.1.0 — Exports successfully, but not with recurring rules

---
+++ export.php
@@ -0,0 +1,135 @@
+<?php
+
+define("PROD_VERSION", "0.0.2");
+
+$events = unserialize(base64_decode($_POST['dataForExport']));
+
+/*echo "<pre>";
+print_r($_POST);
+echo "</pre>";*/
+
+/*echo "<pre>";
+print_r($events);
+echo "</pre>";*/
+
+$filename = "timetable.ics";
+
+//****************************************************************************************
+// Layout and the functions were inspired by Jake Bellacera's PHPtoICS GitHub repository.
+//****************************************************************************************
+
+// Sets the correct header for this file.
+header('Content-type: text/calendar; charset=utf-8');
+header('Content-Disposition: attachment; filename=' . $filename);
+
+// Converts a timestamp to the appropriate string format.
+function dateToCal($timestamp) {
+  return date('Ymd\THis\Z', $timestamp);
+}
+
+// Escapes a string of characters.
+function escapeString($string) {
+  return preg_replace('/([\,;])/','\\\$1', $string);
+}
+
+$offset = 39;
+$startYear = 2016;
+
+// Prints the formatted ICS file content.
+?>
+BEGIN:VCALENDAR
+PRODID:-//Adam Blakey//Scientia Course Planner to ICS <?php echo PROD_VERSION ?>//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:Default Calendar
+X-WR-TIMEZONE:Europe/London
+BEGIN:VTIMEZONE
+TZID:Europe/London
+X-LIC-LOCATION:Europe/London
+BEGIN:DAYLIGHT
+TZOFFSETFROM:+0000
+TZOFFSETTO:+0100
+TZNAME:BST
+DTSTART:19700329T010000
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:+0100
+TZOFFSETTO:+0000
+TZNAME:GMT
+DTSTART:19701025T020000
+RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
+END:STANDARD
+END:VTIMEZONE
+	<?php
+	foreach($events as $singleEvent)
+	{
+		$output = preg_split("/(,|-)/", $singleEvent["weeks"]);
+		$weekStart = str_pad((($offset + $output[0]) % 52), 2, "0", STR_PAD_LEFT);
+		$yearAddOn = floor(($offset + $output[0]) / 52);
+		$year = $startYear + $yearAddOn;
+		$weekStartDate = date("Y-m-d", strtotime($year."W".$weekStart));
+		//echo "<br>".$singleEvent["day"];
+		$DTSTART = strtotime("next ".$singleEvent["day"], strtotime($weekStartDate)) + strtotime("1970-01-01T".$singleEvent["start"]);
+		$DTEND = $DTSTART + strtotime("1970-01-01T".$singleEvent["duration"]);
+		//$singleEvent["weeks"]
+		//echo "<br>STRTOTIME: ".strtotime($year." week ".$weekStart." at ".$singleEvent['start']." on ".$singleEvent['day']);
+		//echo "<br>STRTOTIME: ".strtotime($singleEvent['day'].", ".$weekStartDate);
+		/*echo "<br>";
+		echo "<br>DATE: ".date("Y-m-d", strtotime($year."W".$weekStart));
+		echo "<br>YEAR: ".$year;
+		echo "<br>WEEKSTART: ".$weekStart;
+		echo "<br>Data for STRTOTIME: ".$year."W".$weekStart;
+		echo "<br>STRTOTIME: ".strtotime($year."W".$weekStart);
+		echo "<br><br>";*/
+/*
+?>
+		<br><br>
+		BEGIN:VEVENT
+			<br><b>UID:</b><?= uniqid() ?>
+			<br><b>SUMMARY:</b><?= escapeString($singleEvent["activity"]." — ".$singleEvent["module"]) ?>
+			<br><b>DTSTART:</b><?= DateToCal($DTSTART) ?>
+			<br><b>DTEND:</b><?= dateToCal($DTEND) ?>
+			<br><b>DTSTAMP:</b><?= dateToCal(time()) ?>
+			<br><b>LOCATION:</b><?= escapeString($singleEvent["room"].", University of Nottingham") ?>
+			<br><b>DESCRIPTION:</b><?= escapeString($singleEvent["size"]." person ".strtolower($singleEvent["nameOfType"])." with ".$singleEvent["staff"].", lasting for ".$singleEvent["duration"]." hours.\n\n".$singleEvent["roomDescription"]) ?>
+			<br><b>URL;VALUE=URI:</b><?= escapeString($_POST["URLForImport"]) ?>
+			<br><b>SEQUENCE:</b>
+			<br><b>STATUS:</b></TENTATIVE
+			<br><b>TRANSP:</b></OPAQUE
+			<!-- Try to do this with only BYWEEKNO and not UNTIL. -->
+			<br>RRULE:FREQ=WEEKLY;WKST=MO;BYWEEKNO=<?= escapeString($singleEvent["weeks"]) ?>
+		END:VEVENT
+	<?php
+	*/
+?>
+
+BEGIN:VEVENT
+
+UID:<?= uniqid() ?>
+
+SUMMARY:<?= escapeString($singleEvent["activity"]." — ".$singleEvent["module"]) ?>
+
+DTSTART:<?= DateToCal($DTSTART) ?>
+
+DTEND:<?= dateToCal($DTEND) ?>
+
+DTSTAMP:<?= dateToCal(time()) ?>
+
+LOCATION:<?= escapeString($singleEvent["room"].", University of Nottingham") ?>
+
+DESCRIPTION:<?= escapeString($singleEvent["size"]." person ".strtolower($singleEvent["nameOfType"])." with ".$singleEvent["staff"].", lasting for ".$singleEvent["duration"]." hours.".$singleEvent["roomDescription"]) ?>
+
+URL;VALUE=URI:<?= escapeString($_POST["URLForImport"]) ?>
+
+SEQUENCE:0
+
+STATUS:TENTATIVE
+
+TRANSP:OPAQUE
+
+END:VEVENT
+	<?php } ?>
+
+END:VCALENDAR
--- importer.php
+++ importer.php
@@ -9,72 +9,96 @@
 
 			<input id="pageChooserSubmit" name="pageChooserSubmit" type="submit" value="Find Events" form="pageChooserForm">
 		</form>
-	</body>
-</html>
 
-
-
-
-
-<?php
-// If the form has been submitted:
-if (isset($_POST['pageChooserSubmit']))
-{
-	// Includes file made by Jose Solorzano to extract some of the data from the HTML source of the page provided below.
-	include_once("./simple_html_dom.php");
-
-	// Example page:
-	//$page = file_get_html('http://uiwwwsylp01.nottingham.ac.uk:8001/reporting/TextSpreadsheet;programme+of+study;id;0003091%0D%0A?days=1-5&weeks=1-52&periods=1-32&template=SWSCUST+programme+of+study+TextSpreadsheet&height=100&week=100');
-
-	// Entered page:
-	$page = file_get_html($_POST['pageChooser']);
-
-	echo $page;
-
-	// Finds all data in 'td' HTML elements.
-	$individualCells = $page->find('td');
-
-	// Counts through each of the elements in $individualCells.
-	$arrayCounter = 0;
-
-	// Loops through all elements in $individualCells.
-	foreach($individualCells as $cell)
-	{
-		// If the regular expression is found, is fits the description for an activity name (module code).
-		if (preg_match('/G[0-9][0-9](.*)/', $cell->plaintext))
+		<?php
+		// If the form has been submitted:
+		if (isset($_POST['pageChooserSubmit']))
 		{
-			// Keeps the $arrayCounter value for the start of each module.
-			$eventStartIDs[] = $arrayCounter;
+			// Includes file made by Jose Solorzano to extract some of the data from the HTML source of the page provided below.
+			include_once("./simple_html_dom.php");
+
+			// Example page:
+			//$page = file_get_html('http://uiwwwsylp01.nottingham.ac.uk:8001/reporting/TextSpreadsheet;programme+of+study;id;0003091%0D%0A?days=1-5&weeks=1-52&periods=1-32&template=SWSCUST+programme+of+study+TextSpreadsheet&height=100&week=100');
+
+			// Entered page:
+			$page = file_get_html($_POST['pageChooser']);
+
+			// Finds all data in 'td' HTML elements.
+			$individualCells = $page->find('td');
+
+			// Counts through each of the elements in $individualCells.
+			$arrayCounter = 0;
+
+			// Loops through all elements in $individualCells.
+			foreach($individualCells as $cell)
+			{
+				// If the regular expression is found, is fits the description for an activity name (module code).
+				if (preg_match('/G[0-9][0-9](.*)/', $cell->plaintext))
+				{
+					// Keeps the $arrayCounter value for the start of each module.
+					$eventStartIDs[] = $arrayCounter;
+				}
+
+				$arrayCounter++;
+			}
+
+			// A counter to count through each event.
+			$eventCounter = 0;
+
+			foreach($eventStartIDs as $singleStartID)
+			{
+				$events[$eventCounter]["activity"] = $individualCells[$singleStartID + 0]->plaintext;
+				$events[$eventCounter]["module"] = $individualCells[$singleStartID + 1]->plaintext;
+				$events[$eventCounter]["nameOfType"] = $individualCells[$singleStartID + 2]->plaintext;
+				$events[$eventCounter]["size"] = $individualCells[$singleStartID + 3]->plaintext;
+				$events[$eventCounter]["day"] = $individualCells[$singleStartID + 4]->plaintext;
+				$events[$eventCounter]["start"] = $individualCells[$singleStartID + 5]->plaintext;
+				$events[$eventCounter]["end"] = $individualCells[$singleStartID + 6]->plaintext;
+				$events[$eventCounter]["duration"] = $individualCells[$singleStartID + 7]->plaintext;
+				$events[$eventCounter]["room"] = $individualCells[$singleStartID + 8]->plaintext;
+				$events[$eventCounter]["roomDescription"] = $individualCells[$singleStartID + 9]->plaintext;
+				$events[$eventCounter]["roomSize"] = $individualCells[$singleStartID + 10]->plaintext;
+				$events[$eventCounter]["staff"] = $individualCells[$singleStartID + 11]->plaintext;
+				$events[$eventCounter]["weeks"] = $individualCells[$singleStartID + 12]->plaintext;
+
+				$eventCounter++;
+			}
+
+			echo "Great. We've imported the events. Before proceeding, please verify that the first event is correct:";
+			echo "<ul>";
+				echo "<li><strong>Activity</strong>: ".$events[0]["activity"]."</li>";#
+				echo "<li><strong>Module</strong>: ".$events[0]["module"]."</li>";#
+				echo "<li><strong>Name of Type</strong>: ".$events[0]["nameOfType"]."</li>";#
+				echo "<li><strong>Size</strong>: ".$events[0]["size"]."</li>";#
+				echo "<li><strong>Day</strong>: ".$events[0]["day"]."</li>";
+				echo "<li><strong>Start</strong>: ".$events[0]["start"]."</li>";
+				echo "<li><strong>End</strong>: ".$events[0]["end"]."</li>";
+				echo "<li><strong>Duration</strong>: ".$events[0]["duration"]."</li>";#
+				echo "<li><strong>Room</strong>: ".$events[0]["room"]."</li>";#
+				echo "<li><strong>Room Description</strong>: ".$events[0]["roomDescription"]."</li>";
+				echo "<li><strong>Room size</strong>: ".$events[0]["roomSize"]."</li>";
+				echo "<li><strong>Staff</strong>: ".$events[0]["staff"]."</li>";
+				echo "<li><strong>Weeks</strong>: ".$events[0]["weeks"]."</li>";
+			echo "</ul>";
+
+			// For debugging; shows the entire contents of $event.
+			/*echo "<pre>";
+			print_r($events);
+			echo "</pre>";*/
+
+		?>
+
+		<form id="eventsConfirmedForm" name="eventsConfirmedForm" method="post" action="./export.php" enctype="multipart/form-data">
+			<input id="dataForExport" name="dataForExport" type="hidden" value="<?php echo base64_encode(serialize($events)); ?>">
+			<input id="URLForImport" name="URLForImport" type="hidden" value="<?php echo $_POST['pageChooser']; ?>">
+
+			<input id="eventsConfirmedSubmit" name="eventsConfirmedSubmit" type="submit" value="Confirm Events and Build ICS FIle" form="eventsConfirmedForm">
+		</form>
+	</body>
+</html>	
+
+
+			<?php
 		}
 
-		$arrayCounter++;
-	}
-
-	// A counter to count through each event.
-	$eventCounter = 0;
-
-	foreach($eventStartIDs as $singleStartID)
-	{
-		$event[$eventCounter]["activity"] = $individualCells[$singleStartID + 0]->plaintext;
-		$event[$eventCounter]["module"] = $individualCells[$singleStartID + 1]->plaintext;
-		$event[$eventCounter]["nameOfType"] = $individualCells[$singleStartID + 2]->plaintext;
-		$event[$eventCounter]["size"] = $individualCells[$singleStartID + 3]->plaintext;
-		$event[$eventCounter]["day"] = $individualCells[$singleStartID + 4]->plaintext;
-		$event[$eventCounter]["start"] = $individualCells[$singleStartID + 5]->plaintext;
-		$event[$eventCounter]["end"] = $individualCells[$singleStartID + 6]->plaintext;
-		$event[$eventCounter]["duration"] = $individualCells[$singleStartID + 7]->plaintext;
-		$event[$eventCounter]["room"] = $individualCells[$singleStartID + 8]->plaintext;
-		$event[$eventCounter]["roomDescription"] = $individualCells[$singleStartID + 9]->plaintext;
-		$event[$eventCounter]["roomSize"] = $individualCells[$singleStartID + 10]->plaintext;
-		$event[$eventCounter]["staff"] = $individualCells[$singleStartID + 11]->plaintext;
-		$event[$eventCounter]["weeks"] = $individualCells[$singleStartID + 12]->plaintext;
-
-		$eventCounter++;
-	}
-
-	echo "<pre>";
-	print_r($event);
-	echo "</pre>";
-}
-
-?>
+		?>
