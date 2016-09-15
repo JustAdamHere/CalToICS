90560017794294513f39ab10a730d10db57f9a1c
JustAdamHere <adam@mrmindstorm.co.uk>
2016-09-15 16:01:15 +0100

    0.2.0 — Correctly works (for now)
    Recurring events and excludes appear to now work

--- export.php
+++ export.php
@@ -36,9 +36,6 @@ function formatTime($time) {
 	return str_pad(str_replace(":", "", $time), 4, "0", STR_PAD_LEFT);
 }
 
-$offset = 37;
-$startYear = 2016;
-
 // Prints the formatted ICS file content.
 ?>
 BEGIN:VCALENDAR
@@ -49,7 +46,6 @@ METHOD:PUBLISH
 X-WR-CALNAME:Default Calendar
 X-WR-TIMEZONE:Europe/London
 
-<?php if(false) { ?>
 BEGIN:VTIMEZONE
 TZID:Europe/London
 X-LIC-LOCATION:Europe/London
@@ -68,18 +64,22 @@ DTSTART:19701025T020000
 RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
 END:STANDARD
 END:VTIMEZONE
-<?php } ?>
-	<?php
-	date_default_timezone_set("Europe/London");
-	foreach($events as $singleEvent)
+
+<?php
+	function createSingleEvent($singleEvent)
 	{
+		date_default_timezone_set("Europe/London");
+
+		$offset = 38;
+		$startYear = 2016;
+
 		$output = preg_split("/(,|-)/", $singleEvent["weeks"]);
 		$weekStart = str_pad((($offset + $output[0]) % 52), 2, "0", STR_PAD_LEFT);
 		$yearAddOn = floor(($offset + $output[0]) / 52);
 		$year = $startYear + $yearAddOn;
 		$weekStartDate = date("Y-m-d", strtotime($year."W".$weekStart));
 		//echo "<br>".$singleEvent["day"];
-		$DTSTART = strtotime("next ".$singleEvent["day"], strtotime($weekStartDate)) + strtotime("19700101T".formatTime($singleEvent["start"])."00Z");
+		$DTSTART = strtotime("next ".$singleEvent["day"], strtotime($weekStartDate) - 2*60*60*24) + strtotime("19700101T".formatTime($singleEvent["start"])."00Z"); // Takes 2 days off $weekStartDate so that the first day of the week is always included.
 		$DTEND = $DTSTART + strtotime("19700101T".formatTime($singleEvent["duration"])."00Z");
 
 		/*$splitIndividuals = explode(",", $singleEvent["weeks"])
@@ -101,89 +101,79 @@ END:VTIMEZONE
 		}, $singleEvent["weeks"]));
 		// End of Routine by hakre
 
+		$firstWeekNumber = min($weekNumbers);
+		$lastWeekNumber = max($weekNumbers);
+
+		$exdates = array();
+
+		for($i = $firstWeekNumber; $i <= $lastWeekNumber; $i++)
+		{
+			if (!(in_array($i, $weekNumbers)))
+			{
+				$weekNumber = str_pad((($offset + $i) % 52), 2, "0", STR_PAD_LEFT);
+				$yearAddOn = floor(($offset + $i) / 52);
+				$year = $startYear + $yearAddOn;
+				$excludedWeekTimestamp = strtotime(date("Y-m-d", strtotime($year."W".$weekNumber)));
+
+				$excludedEventDate = strtotime("next ".$singleEvent["day"], $excludedWeekTimestamp - 2*60*60*24) + strtotime("19700101T".formatTime($singleEvent["start"])."00Z"); // Takes 2 days off $weekStartDate so that the first day of the week is always included.
+
+				$exdates[] = $excludedEventDate;
+			}
+		}
+
 		// Repeats to:
 		$weekEnd = str_pad((($offset + max($weekNumbers)) % 52), 2, "0", STR_PAD_LEFT);
 		$yearAddOn = floor(($offset + max($weekNumbers)) / 52);
 		$year = $startYear + $yearAddOn;
 		$weekEndDate = date("Y-m-d", strtotime($year."W".$weekEnd));
-		$WEEKUNTIL = strtotime("next ".$singleEvent["day"], strtotime($weekEndDate) + 60*60*24); // Adds a day after the UNTIL, so that the last event will definitely be included.
+		$WEEKUNTIL = strtotime("next ".$singleEvent["day"], strtotime($weekEndDate) + (7-1)*24*60*60); // Adds on a week, less a day so that the final week's events are included.
 
+		$outputForFunction = "";
 
-		//echo "<br>STRTOTIME: ".strtotime($year." week ".$weekStart." at ".$singleEvent['start']." on ".$singleEvent['day']);
-		//echo "<br>STRTOTIME: ".strtotime($singleEvent['day'].", ".$weekStartDate);
-		/*echo "<br>";
-		echo "<br>DATE: ".date("Y-m-d", strtotime($year."W".$weekStart));
-		echo "<br>YEAR: ".$year;
-		echo "<br>WEEKSTART: ".$weekStart;
-		echo "<br>Data for STRTOTIME: ".$year."W".$weekStart;
-		echo "<br>STRTOTIME: ".strtotime($year."W".$weekStart);
-		echo "<br><br>";*/
-/*
-?>
-		<br><br>
-		BEGIN:VEVENT
-			<br><b>UID:</b><?= uniqid() ?>
-			<br><b>SUMMARY:</b><?= escapeString($singleEvent["activity"]." — ".$singleEvent["module"]) ?>
-			<br><b>DTSTART:</b><?= DateToCal($DTSTART) ?>
-			<br><b>DTEND:</b><?= dateToCal($DTEND) ?>
-			<br><b>DTSTAMP:</b><?= dateToCal(time()) ?>
-			<br><b>LOCATION:</b><?= escapeString($singleEvent["room"].", University of Nottingham") ?>
-			<br><b>DESCRIPTION:</b><?= escapeString($singleEvent["size"]." person ".strtolower($singleEvent["nameOfType"])." with ".$singleEvent["staff"].", lasting for ".$singleEvent["duration"]." hours.\n\n".$singleEvent["roomDescription"]) ?>
-			<br><b>URL;VALUE=URI:</b><?= escapeString($_POST["URLForImport"]) ?>
-			<br><b>SEQUENCE:</b>
-			<br><b>STATUS:</b></TENTATIVE
-			<br><b>TRANSP:</b></OPAQUE
-			<!-- Try to do this with only BYWEEKNO and not UNTIL. -->
-			<br>RRULE:FREQ=WEEKLY;WKST=MO;BYWEEKNO=<?= escapeString($singleEvent["weeks"]) ?>
-		END:VEVENT
-	<?php
-	*/
-?>
+		$outputForFunction .= "BEGIN:VEVENT\n";
 
-BEGIN:VEVENT
+		$outputForFunction .= "DTSTART;TZID=Europe/London:".dateToCal($DTSTART)."\n";
 
-DTSTART;TZID=Europe/London:<?= DateToCal($DTSTART) ?>
+		$outputForFunction .= "DTEND;TZID=Europe/London:".dateToCal($DTEND)."\n";
 
-DTEND;TZID=Europe/London:<?= dateToCal($DTEND) ?>
+		$outputForFunction .= "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=".dateToCal($WEEKUNTIL).";BYDAY=".strtoupper(substr(date("D", $DTSTART), 0, 2))."\n";
 
-RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=<?= dateToCal($WEEKUNTIL) ?>;BYDAY=<?= strtoupper(substr(date("D", $DTSTART), 0, 2)) ?>
+		//if (false)
+		foreach($exdates as $excludedDate)
+		{
+			$outputForFunction .= "EXDATE;TZID=Europe/London:".dateToCal($excludedDate)."\n";
+		}
 
-<?php if(false)
-{ ?>
-	EXDATE;TZID=Europe/London:20131026T090000
-<?php } ?>
+		$outputForFunction .= "DTSTAMP:".dateToCal(time())."\n";
 
-DTSTAMP:<?= dateToCal(time()) ?>
+		$outputForFunction .= "UID:".uniqid()."\n";
 
-UID:<?= uniqid() ?>
+		$outputForFunction .= "CREATED:".dateToCal(time())."\n";
 
-CREATED<?= dateToCal(time()) ?>
+		$outputForFunction .= "DESCRIPTION:".escapeString($singleEvent["size"]." person ".strtolower($singleEvent["nameOfType"])." with ".$singleEvent["staff"].", lasting for ".$singleEvent["duration"]." hours.".$singleEvent["roomDescription"])."\n";
 
-DESCRIPTION:<?= escapeString($singleEvent["size"]." person ".strtolower($singleEvent["nameOfType"])." with ".$singleEvent["staff"].", lasting for ".$singleEvent["duration"]." hours.".$singleEvent["roomDescription"]) ?>
+		$outputForFunction .= "LAST-MODIFIED:".dateToCal(time())."\n";
 
-LAST-MODIFIED:<?= dateToCal(time()) ?>
+		$outputForFunction .= "LOCATION:".escapeString($singleEvent["room"].", University of Nottingham")."\n";
 
-LOCATION:<?= escapeString($singleEvent["room"].", University of Nottingham") ?>
+		$outputForFunction .= "SEQUENCE:0"."\n";
 
-SEQUENCE:0
+		$outputForFunction .= "STATUS:TENTATIVE"."\n";
 
-STATUS:TENTATIVE
+		$outputForFunction .= "SUMMARY: ".escapeString($singleEvent["activity"]." — ".$singleEvent["module"])."\n";
 
-SUMMARY:<?= escapeString($singleEvent["activity"]." — ".$singleEvent["module"]) ?>
+		$outputForFunction .= "TRANSP:OPAQUE"."\n";
 
-TRANSP:OPAQUE
+		if(false) { $outputForFunction .= "URL;VALUE=URI:".escapeString($_POST["URLForImport"])."\n"; }
 
-<?php if(false) { ?>URL;VALUE=URI:<?= escapeString($_POST["URLForImport"])?><?php } ?>
+		$outputForFunction .= "END:VEVENT"."\n\n";
 
+		return $outputForFunction;
+	}
 
-
-
-
-
-
-
-
-END:VEVENT
-	<?php } ?>
+	foreach($events as $singleEvent)
+	{
+		echo createSingleEvent($singleEvent);
+	} ?>
 
 END:VCALENDAR
